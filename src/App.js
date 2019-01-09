294b3f2eec5a3719625e3d4d517cdc7709d99c6a
vuongvu <hoangvuong.v99@gmail.com>
2019-01-09 00:59:46 -0800

    Add router, redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
+import { simpleAction } from './actions/simpleAction'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+import './App.scss';
+
+const App = (props) => {
+  const { result } = props;
+  return (
+    <div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">{ result }</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
 }
 
-export default App;
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+const mapStateToProps = state => ({
+  ...state.simpleReducer
+ });
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+ });
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/reducers/simpleReducer.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+  result: 'hello'
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'SIMPLE_ACTION':
+      return {
+        result: action.payload
+      }
+    default:
+      return state
+  }
+}
